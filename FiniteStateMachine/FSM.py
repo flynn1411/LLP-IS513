7d3a2f67a79088d1144025d04e9ba73f9202797c
Josue Izaguirre <flynn1411@gmail.com>
2020-07-14 22:49:55 -0600

    FSM commit

---
+++ FiniteStateMachine/FSM.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class FiniteStateMachine:
+    def __init__(self): pass
+
+    #crear una representación de estados
+
+    #Comenzar la máquina
+    def run(self): pass
+
+    #método para cambiar de estados
+    def changeState(self): pass
